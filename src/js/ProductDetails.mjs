56483b46b45178f8d95b9fdec4da0846db7ddd15
Juan Plasencia <jplasencia.dev@outlook.com>
2024-05-08 14:25:17 -0500

    W02 Team Activity: Dynamic product detail pages completed

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,45 @@
+import { setLocalStorage } from "./utils.mjs";
+
+const productDetailsTemplate = product => `
+    <section class="product-detail">
+        <h3>${product.Brand.Name}</h3>
+        <h2 class="divider">${product.NameWithoutBrand}</h2>
+        <img class="divider" src="${product.Image}" alt="${product.NameWithoutBrand}"/>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+        <p class="product__color">${product.Colors[0].ColorName}</p>
+        <p class="product__description">
+        ${product.DescriptionHtmlSimple}
+        </p>
+        <div class="product-detail__add">
+            <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+        </div>
+    </section>
+  `;
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+    
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+      this.renderProductDetails("main");
+    document.getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
+  }
+    
+  addToCart() {
+    setLocalStorage("so-cart", this.product)
+  }
+
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+        "afterBegin",
+        productDetailsTemplate(this.product)
+    );
+  }
+    
+}
