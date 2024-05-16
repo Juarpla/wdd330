3f24621229e254c724a1d3c304f73e71c3f21b5e
Julirey <reyes.0271204@gmail.com>
2024-05-15 22:59:43 -0400

    Add steps 4 ~ 6

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,10 +9,7 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
+  constructor(category) {}
 
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
@@ -21,7 +18,8 @@ export default class ProductData {
   }
 
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
 }
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,7 +5,7 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 loadHeaderFooter();
 
 const category = getParam("category");
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
