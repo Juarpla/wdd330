dd2f4f700581a364d7e55dc71c30a992021e80f3
Julirey <reyes.0271204@gmail.com>
2024-05-17 02:09:04 -0400

    Add team activity 4 | steps 1 ~ 4 (and various optimizations)

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,31 +1,8 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 const listing = new ShoppingCart("so-cart", ".product-list");
 
-function getTotal() {
-  const cartItems = getLocalStorage("so-cart");
-  if (cartItems) {
-    const prices = [];
-    cartItems.map((item) => addToArray(item, prices));
-
-    let sum = 0;
-    for (let i = 0; i < prices.length; i++) {
-      sum += prices[i];
-    }
-
-    document.querySelector(".cart-total").textContent = `Total: $${sum}`;
-  } else {
-    document.querySelector(".cart-footer").style.display = "none";
-  }
-}
-
-function addToArray(item, array) {
-  array.push(item.FinalPrice);
-}
-
 loadHeaderFooter();
 
 listing.renderCartContents();
-
-getTotal();
---
+++ src/js/checkout.js
@@ -0,0 +1,17 @@
+import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
+
+loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+myCheckout.init();
+
+document
+  .querySelector("#zip")
+  .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
+
+// event listener
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+  myCheckout.checkout();
+});
