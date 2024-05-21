55fdb0d6f5d5ceb570c06108353e6e4b1b9721d9
Julirey <reyes.0271204@gmail.com>
2024-05-20 23:04:06 -0400

    Add team activity 4 | steps 5 ~ 6 | finish

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,12 +1,12 @@
 import Alert from "./Alert";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const category = getParam("category");
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
 import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 // get the product id successfully when someone navigates to the product-details page
 const productId = getParam("product");
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 
 const product = new ProductDetails(productId, dataSource);
 
